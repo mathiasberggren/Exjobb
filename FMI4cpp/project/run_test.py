4098cb763ce6cf4804b5e8049e057abc72821a18
Mathias Berggren <matbe320@student.liu.se>
2020-01-24 09:34:01 +0100

    Added START_ANGLE in python script

--- FMI4cpp/project/run_test.py
+++ FMI4cpp/project/run_test.py
@@ -8,19 +8,19 @@ import pandas as pd
 
 
 def simulate_all_pendulums():
-    program_specific = ['TRAINING_MODE=0']
-    pendulum_start_angle = ['3.14']
+    base = ['TRAINING_MODE=0']
+    pendulum_start_angle = ['7.6', '5.4', '4.3', '17.12', '3.2', '5.6', '3.4', '2.3', '7.12', '1.2']
     pendulum_m1 = [1.0]
     pendulum_m2 = [0.3] #, 0.6, 1.5]
     pendulum_length = [0.5] #, 1.0] 
 
     # for loop this shit
     for start_value in pendulum_start_angle:
-        # Generate some kind of pendulum model with different parameters
         for p_m1 in pendulum_m1:
             for p_l in pendulum_length:
                 for p_m2 in pendulum_m2:
                     directory = './test_script/M'+str(p_m1)+'L'+str(p_l)+'m'+str(p_m2)
+                    program_specific = base + ['START_ANGLE='+start_value, 'DIRECTORY='+directory]
                     # Create directory if does not exist
                     call(['mkdir', directory])
                     # Run program with all different pendulums
@@ -31,11 +31,11 @@ def simulate_all_pendulums():
 def run_subprogram(path_to_exec, directory, program_specific = None):
     """ Takes in the path to the executable, directory where result is saved and program specific constants """
     program = path_to_exec
-    controllers = [0, 1, 2, 3]
+    controllers = [0, 1]
     for controller in controllers:
         call(['make', 'clean'])
-        # '-s' suppresses warnings from matplotlib
-        call(['make', '-s', 'CONTROLLER_MODE=' + str(controller), "DIRECTORY=" + directory,'FILENAME=' + path_to_exec + '-' + str(controller)]+program_specific)
+        # '-s' make output is silent
+        call(['make', '-s', 'CONTROLLER_MODE=' + str(controller), 'FILENAME=' + path_to_exec + '-' + str(controller)]+program_specific)
         call(['./'+str(path_to_exec)+'-'+str(controller)+'-0'])
 
 
